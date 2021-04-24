aae5d0a2a2a392d0bdf4bd0b6d2802dd535d30c7
Kemal Muharemovic <45690410+KemalMuharemovic@users.noreply.github.com>
2021-04-24 16:23:28 +0200

    Upload #1
    All codes without an image folder.

---
+++ js/accordion.js
@@ -0,0 +1,39 @@
+var acc = document.getElementsByClassName("dropdownBtn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+//footer js
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel12 = this.nextElementSibling;
+    if (panel12.style.display === "block") {
+      panel12.style.display = "none";
+    } else {
+      panel12.style.display = "block";
+    }
+  });
+}
